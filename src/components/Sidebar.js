fbe4769b26c61dfcb9ee6fb0522dce6a0538d9c9
Isaacllolo <isaaacllolo@gmail.com>
2021-10-16 15:23:33 -0500

    Sidebar_init

---
+++ src/components/Sidebar.js
@@ -0,0 +1,76 @@
+import React from "react";
+import './Sidebar.css';
+class Sidebar extends React.Component{
+render(){
+    return(
+    <div className="sidebar">
+          <div className="person-_screen">
+            <div className="sidebar">
+              <div className="b-g" />
+              <div className="new-_pacient" />
+              <div className="header">
+                <div className="b-g-header" />
+                <div className="images-/-avatar">
+                  <div className="images-/-avatar">
+                    <div className="mask" />
+                    <div className="mask" />
+                    <img src="" />
+                  </div>
+                </div>
+                <p className="text-2">Edad</p>
+                <p className="text-3">48</p>
+                <p className="text-4">Rachel Brown</p>
+                <div className="menu">
+                  <img src="" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="user">
+            <button className="button-/-sidebar-/-active">
+              <p className="text-6">Agregar cuidador</p>
+              <img src="" />
+            </button>
+          </div>
+          <div className="user">
+            <div className="user-_-1">
+              <button className="button-/-sidebar-/-active">
+                <p className="text-8">Rachel Brown</p>
+                <div className="images-/-avatar">
+                  <div className="mask" />
+                  <div className="mask" />
+                  <img src="" />
+                </div>
+              </button>
+            </div>
+          </div>
+          <div className="user">
+            <button className="button-/-sidebar-/-active">
+              <div className="rectangle" />
+              <p className="text-1-0">Lady Does</p>
+              <div className="images-/-avatar">
+                <div className="mask" />
+                <div className="mask" />
+                <img src="" />
+              </div>
+            </button>
+          </div>
+          <div className="user">
+            <div className="new-_pacient">
+              <button className="button-/-sidebar-/-active">
+                <div className="rectangle" />
+                <p className="text-1-2">Agregar paciente</p>
+                <img src="" />
+              </button>
+            </div>
+          </div>
+        </div>
+      )
+    
+    
+    
+    
+ 
+}
+}
+export default Sidebar
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Sidebar from './components/Sidebar';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Sidebar />
   </React.StrictMode>,
   document.getElementById('root')
 );
