6e7bcab43fdafcb7728bb914364e6584bb2c6aca
FrigoAlex <alejo012903@hotmail.com>
2021-11-13 04:05:03 -0500

    Registrar paciente

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,12 +8,12 @@ const Login = () => {
         <Form>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="Email" />
+                <Form.Control type="email" placeholder="Email"/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Contraseña</Form.Label>
-                <Form.Control type="password" placeholder="Contraseña" />
+                <Form.Control type="password" placeholder="Contraseña"/>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
                 <Form.Label>¿No estas registrado? <Link to="/reg">registrate aquí</Link></Form.Label>            
--- src/components/P_reg.js
+++ src/components/P_reg.js
@@ -1,9 +1,44 @@
 import React from "react";
+import { Row, Col, Image, Button, Form } from 'react-bootstrap';
 const P_reg = () => {
     return(
-        <div>
-         Registre paciente
-        </div>
+        <div className="container">
+        <h2 className="fw-bold text-center">Paciente</h2>
+        <Form>
+            <Row>
+                <Col className="text-center">
+                    <Image className="my-1" src="UserIcon.png" roundedCircle/>
+                </Col>
+            </Row>
+            <Form.Group className="mb-3">
+                <Form.Label>Nombre</Form.Label>
+                <Form.Control type="text" placeholder="Nombre"/>
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label>Teléfono</Form.Label>
+                <Form.Control type="text" placeholder="Teléfono"/>
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label>EPS</Form.Label>
+                <Form.Control type="text" placeholder="EPS"/>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email EPS</Form.Label>
+                <Form.Control type="email" placeholder="Email EPS"/>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="Email" />
+            </Form.Group>
+
+            <div className="text-center">
+            <Button variant="primary" type="submit">
+                Registrar
+            </Button>
+            </div>
+            
+        </Form>
+    </div>
       
 
     
