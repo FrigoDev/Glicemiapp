ffc75a036a0c0d891621dbff70663172c2348557
FrigoAlex <alejo012903@hotmail.com>
2021-11-13 16:00:06 -0500

    Diseños mejorados Login, Reg, P_reg

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,31 +1,35 @@
 import React from "react";
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 const Login = () => {
     return (
-    <div className="container">
+    <Card className="mx-auto" style={{ width: '20rem' }}>
+        <Card.Header>
         <h1 className="fw-bold text-center">GlicDiary</h1>
-        <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="Email"/>
-            </Form.Group>
+        </Card.Header>
+        <Card.Body>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" placeholder="Email"/>
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Contraseña</Form.Label>
-                <Form.Control type="password" placeholder="Contraseña"/>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Label>¿No estas registrado? <Link to="/reg">registrate aquí</Link></Form.Label>            
-            </Form.Group>
-            <div className="text-center">
-            <Button variant="primary" type="submit">
-                Iniciar Sesión
-            </Button>
-            </div>
-            
-        </Form>
-    </div>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Contraseña</Form.Label>
+                    <Form.Control type="password" placeholder="Contraseña"/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Label>¿No estas registrado? <Link to="/reg">registrate aquí</Link></Form.Label>            
+                </Form.Group>
+                <div className="text-center">
+                <Button variant="primary" type="submit">
+                    Iniciar Sesión
+                </Button>
+                </div>
+                
+            </Form>
+        </Card.Body>
+    </Card>
     );
 }
 export default Login;
