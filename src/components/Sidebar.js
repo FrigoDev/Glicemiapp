b38f67d0873b01874bab033af574dce915676cd0
Isaacllolo <isaaacllolo@gmail.com>
2021-11-13 00:25:03 -0500

    Sidebar
    La sidebar esta lista y funciona pero su boton quedaba dentro del body entonces estoy buscando como enlazar ese hooks con ese boton del navbar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,30 +1,70 @@
-import React from "react";
-
-import './Sidebar.css';
-import { NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import { SidebarData } from './SidebarData';
+import { IconContext } from 'react-icons/lib';
+import SubMenu from './SubMenu';
+const Nav = styled.div`
+  height: 80px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const NavIcon = styled(Link)`
+  margin-left: 2rem;
+  font-size: 2rem;
+  height: 80px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const SidebarNav = styled.nav`
+  background: #15171c;
+  width: 250px;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: ${({ sidebar }) => (sidebar ? '0' : '-100%')};
+  transition: 350ms;
+  z-index: 10;
+`;
+
+const SidebarWrap = styled.div`
+  width: 100%;
+`;
 
 const Sidebar = () => {
-    return(  
-    <div className='sidebar bg-light'>
-        <ul>
-            <li>
-                <NavLink to="/home"exact className='text-dark rounded py-2 w-100 d-inline-block px-2'activeClassName='active' >
-                <FaIcons.FaUser className='me-2'/> Paciente
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to="/p_reg"exact className='text-dark rounded py-2 w-100 d-inline-block px-2'activeClassName='active' >
-                <FaIcons.FaUserPlus className='me-2c'/> Agregar paciente
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to="/reg"exact className='text-dark rounded py-2 w-100 d-inline-block px-2'activeClassName='active' >
-                <FaIcons.FaDoorClosed className='me-2c'/>  Cerrar sesion
-                </NavLink>
-            </li>
-        </ul>
-    </div>  
-    );
-}
-export default Sidebar
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
+
+  return (
+    <>
+    
+      <IconContext.Provider value={{ color: '#fff' }}>
+        <Nav>
+          <NavIcon to='#'>
+          </NavIcon>
+        </Nav>
+        <SidebarNav sidebar={sidebar}>
+          <SidebarWrap>
+            <NavIcon to='#'>
+              <AiIcons.AiOutlineClose onClick={showSidebar} />
+            </NavIcon>
+            {SidebarData.map((item, index) => {
+              return <SubMenu item={item} key={index} />;
+            })}
+          </SidebarWrap>
+        </SidebarNav>
+      </IconContext.Provider>
+    </>
+  );
+};
+
+export default Sidebar;
---
+++ src/components/loc.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import './Sidebar.css';
+import { NavLink } from 'react-router-dom';
+import * as FaIcons from 'react-icons/fa';
+
+const Sidebar = () => {
+
+    return(  
+    <div className='sidebar bg-light'>
+        <ul>
+            
+            <li>
+                <NavLink to="/home"exact className='text-dark rounded py-2 w-100 d-inline-block px-2'activeClassName='active' >
+                <FaIcons.FaUser className='me-2'/> Paciente
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to="/p_reg"exact className='text-dark rounded py-2 w-100 d-inline-block px-2'activeClassName='active' >
+                <FaIcons.FaUserPlus className='me-2c'/> Agregar paciente
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to="/reg"exact className='text-dark rounded py-2 w-100 d-inline-block px-2'activeClassName='active' >
+                <FaIcons.FaDoorClosed className='me-2c'/>  Cerrar sesion
+                </NavLink>
+            </li>
+        </ul>
+    </div>  
+    );
+}
+export default Sidebar
