bbcf0b8adee75e934383b84f037fe6f5c353110a
Isaacllolo <isaaacllolo@gmail.com>
2021-11-14 14:16:18 -0500

    Pacientes

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -8,25 +8,25 @@ export const SidebarData = [
   {
     title: 'Pacientes',
     path: '/home',
-    icon: <AiIcons.AiFillHome />,
+    icon: <FaIcons.FaUserCircle className=' fa-6x text-white' />,
     iconClosed: <RiIcons.RiArrowDownSFill />,
     iconOpened: <RiIcons.RiArrowUpSFill />,
 
     
   },
   {
-    title: 'Reports',
+    title: 'Registrar paciente',
     path: '/p_reg',
-    icon: <IoIcons.IoIosPaper />,
+    icon: <FaIcons.FaUserPlus className=' fa-6x text-white' />,
     iconClosed: <RiIcons.RiArrowDownSFill />,
     iconOpened: <RiIcons.RiArrowUpSFill />,
 
    
   },
   {
-    title: 'Cerrar sesionS',
+    title: 'Cerrar sesion',
     path: '/reg',
-    icon: <FaIcons.FaCartPlus />
+    icon: <FaIcons.FaDoorClosed className=' fa-6x text-white' />
   },
   {
     title: 'Team',
--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,66 @@
 import React from "react";
+import {  Card } from 'react-bootstrap';
+import * as FaIcons from 'react-icons/fa';
+
 const Home = () => {
     return(
-        <div>
-         HOME
-        </div>
-      
+        <Card className="mx-auto" style={{ width: '20rem' }}>
+        <Card.Header>
+        <h2 className="fw-bold text-center">Pacientes</h2>
+        </Card.Header>
+        <Card.Body>
+        <div class="card-block px-0 py-3">
+                                            
+                                                <table class="table table-hover">
+                                                    <tbody>
+                                                        <tr class="unread">
+                                                            <td><FaIcons.FaUserCircle className=' fa-5x text-black' /></td>
+                                                            <td>
+                                                                <h6 class="mb-1">Isabella Christensen</h6>
+                                                                <p class="m-0">Lorem Ipsum is simply…</p>
+                                                            </td>
+                                                            
+                                                        </tr>
+                                                        <tr class="unread">
+                                                        <td><FaIcons.FaUserCircle className=' fa-5x text-black' /></td>
+                                                            <td>
+                                                                <h6 class="mb-1">Mathilde Andersen</h6>
+                                                                <p class="m-0">Lorem Ipsum is simply text of…</p>
+                                                            </td>
+                                                            
+                                                        </tr>
+                                                        <tr class="unread">
+                                                        <td><FaIcons.FaUserCircle className=' fa-5x text-black' /></td>
+                                                            <td>
+                                                                <h6 class="mb-1">Karla Sorensen</h6>
+                                                                <p class="m-0">Lorem Ipsum is simply…</p>
+                                                            </td>
+                                                            
+                                                        </tr>
+                                                        <tr class="unread">
+                                                        <td><FaIcons.FaUserCircle className=' fa-5x text-black' /></td>
+                                                            <td>
+                                                                <h6 class="mb-1">Ida Jorgensen</h6>
+                                                                <p class="m-0">Lorem Ipsum is simply text of…</p>
+                                                            </td>
+                                                            
+                                                        </tr>
+                                                        <tr class="unread">
+                                                        <td><FaIcons.FaUserCircle className=' fa-5x text-black' /></td>
+                                                            <td>
+                                                                <h6 class="mb-1">Albert Andersen</h6>
+                                                                <p class="m-0">Lorem Ipsum is simply dummy…</p>
+                                                            </td>
+
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                     
+        </Card.Body>
+</Card> 
+                                        
+                                  
 
     
     
