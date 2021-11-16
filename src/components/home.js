3191a1712bcf3b214ae10343a33579d04580fdd3
NokiaXD <38729483+NokiaXD@users.noreply.github.com>
2021-11-15 23:37:54 -0500

    relación paciente-cuidador

--- src/components/home.js
+++ src/components/home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { Card } from 'react-bootstrap';
 import * as FaIcons from 'react-icons/fa';
 import { useHistory } from 'react-router-dom';
@@ -19,11 +19,13 @@ const PData = (props) => {
 }
 
 const Home = () => {
+        const [data, setData] = useState([]);
         const history = useHistory();
         const envio = async () => {
             await axios.post("http://192.168.1.3:4000/subscription", { "subs": JSON.parse(localStorage.getItem('subs')), "email": localStorage.getItem("correo") })
+            const datos = await axios.post("http://192.168.1.3:4000/Getacientes",{email: localStorage.getItem("correo")})
+            setData(datos.data);
         }
-
         if (localStorage.getItem('correo') === null) {
             history.push('/login');
         }
@@ -31,7 +33,14 @@ const Home = () => {
             if (localStorage.getItem('tipo') === '0') {
                 return (<SetType />)
             }
-            envio();
+            else if(localStorage.getItem('tipo') === '2'){
+                if(data.length === 0)
+                {
+                    envio();
+                } 
+                
+            }
+            
         }
 
         return (
@@ -44,44 +53,13 @@ const Home = () => {
 
                         <table class="table table-hover">
                             <tbody>
+                                {
+                                    data.map((item,i) => 
+                                    {
+                                        return <PData key={i} name={item.nombre} description={`${item.cedula}`}/>
+                                    })
+                                }
 
-
-                                <PData name="Isaac" description="Soy un trollazo"></PData>
-
-
-
-                                <tr class="unread">
-                                    <td><FaIcons.FaUserCircle className=' fa-5x text-black' /></td>
-                                    <td>
-                                        <h6 class="mb-1">Mathilde Andersen</h6>
-                                        <p class="m-0">Lorem Ipsum is simply text of…</p>
-                                    </td>
-
-                                </tr>
-                                <tr class="unread">
-                                    <td><FaIcons.FaUserCircle className=' fa-5x text-black' /></td>
-                                    <td>
-                                        <h6 class="mb-1">Karla Sorensen</h6>
-                                        <p class="m-0">Lorem Ipsum is simply…</p>
-                                    </td>
-
-                                </tr>
-                                <tr class="unread">
-                                    <td><FaIcons.FaUserCircle className=' fa-5x text-black' /></td>
-                                    <td>
-                                        <h6 class="mb-1">Ida Jorgensen</h6>
-                                        <p class="m-0">Lorem Ipsum is simply text of…</p>
-                                    </td>
-
-                                </tr>
-                                <tr class="unread">
-                                    <td><FaIcons.FaUserCircle className=' fa-5x text-black' /></td>
-                                    <td>
-                                        <h6 class="mb-1">Albert Andersen</h6>
-                                        <p class="m-0">Lorem Ipsum is simply dummy…</p>
-                                    </td>
-
-                                </tr>
                             </tbody>
                         </table>
                     </div>
