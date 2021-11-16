90c22e285e2f0cb7e24381046ebb517b20da1764
FrigoAlex <alejo012903@hotmail.com>
2021-11-15 19:15:59 -0500

    Propos Diario y Home

--- src/components/diario.js
+++ src/components/diario.js
@@ -2,6 +2,17 @@ import React from "react";
 import { Row, Col, Image, Button, Form, FormControl, InputGroup, Card } from 'react-bootstrap';
 import './diario.css'
 
+const PData = (props) => {
+    return (
+        <tr>
+            <th className="fw-normal" scope="row">{props.fecha}</th>
+            <td>{props.hora}</td>
+            <td>{props.glucosa}</td>
+            <td>{props.Insulina}</td>
+        </tr>
+    );
+}
+
 
 
 const diario = () => {
@@ -38,39 +49,31 @@ const diario = () => {
                             <tr>
                                 <th scope="col">Fecha</th>
                                 <th scope="col">Hora</th>
-                                <th scope="col">Glucosa</th>
-                                <th scope="col">Insulina</th>
-                                <th scope="col">Medicamento</th>
+                                <th scope="col">Glucosa(mg/dl)</th>
+                                <th scope="col">Insulina(Bolo)</th>
                             </tr>
                         </thead>
                         <tbody>
+
+                            <PData fecha="10/11" hora="10:00" glucosa="100" Insulina="1" />
+
                             <tr>
-                                <th scope="row">1</th>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
+                                <th className="fw-normal" scope="row">12/11</th>
+                                <td>18:00</td>
+                                <td>130</td>
+                                <td>10</td>
                             </tr>
                             <tr>
-                                <th scope="row">2</th>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
+                                <th className="fw-normal" scope="row">13/11</th>
+                                <td>20:00</td>
+                                <td>90</td>
+                                <td>0</td>
                             </tr>
                             <tr>
-                                <th scope="row">3</th>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <th scope="row">4</th>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
+                                <th className="fw-normal" scope="row">14/11</th>
+                                <td>8:00</td>
+                                <td>30</td>
+                                <td>0</td>
                             </tr>
                         </tbody>
 
--- src/components/home.js
+++ src/components/home.js
@@ -1,87 +1,93 @@
 import React from "react";
 import { Card } from 'react-bootstrap';
 import * as FaIcons from 'react-icons/fa';
-import  {useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 import SetType from './set_tipo';
 
-const Home = () => {
-    const history = useHistory();
-    const envio = async() => {
-        await axios.post("http://192.168.1.3:4000/subscription",{"subs":JSON.parse(localStorage.getItem('subs')),"email":localStorage.getItem("correo")} )  
-    } 
-
-    if(localStorage.getItem('correo') === null){
-        history.push('/login');
-    }
-    else{
-        if(localStorage.getItem('tipo')==='0')
-        {
-            return(<SetType/>) 
-        } 
-        envio();
-    } 
-
+//recibir props y retornar una vista
+const PData = (props) => {
     return (
-        <Card className="mx-auto" style={{ width: '20rem'}}>
-            <Card.Header>
-                <h2 className="fw-bold text-center">Pacientes</h2>
-            </Card.Header>
-            <Card.Body>
-                <div class="card-block px-0 py-3">
-
-                    <table class="table table-hover">
-                        <tbody>
-                            
-                                
-                                <tr class="unread" >
-                                    <td><FaIcons.FaUserCircle className=' fa-5x text-black' /></td>
-                                <td>
-                                    <h6 class="mb-1">Isabella Christensen</h6>
-                                    <p class="m-0">Lorem Ipsum is simply…</p>
-                                </td>
-                                   </tr>
-                                
-                     
-                         
-                            <tr class="unread">
-                                <td><FaIcons.FaUserCircle className=' fa-5x text-black' /></td>
-                                <td>
-                                    <h6 class="mb-1">Mathilde Andersen</h6>
-                                    <p class="m-0">Lorem Ipsum is simply text of…</p>
-                                </td>
-
-                            </tr>
-                            <tr class="unread">
-                                <td><FaIcons.FaUserCircle className=' fa-5x text-black' /></td>
-                                <td>
-                                    <h6 class="mb-1">Karla Sorensen</h6>
-                                    <p class="m-0">Lorem Ipsum is simply…</p>
-                                </td>
-
-                            </tr>
-                            <tr class="unread">
-                                <td><FaIcons.FaUserCircle className=' fa-5x text-black' /></td>
-                                <td>
-                                    <h6 class="mb-1">Ida Jorgensen</h6>
-                                    <p class="m-0">Lorem Ipsum is simply text of…</p>
-                                </td>
-
-                            </tr>
-                            <tr class="unread">
-                                <td><FaIcons.FaUserCircle className=' fa-5x text-black' /></td>
-                                <td>
-                                    <h6 class="mb-1">Albert Andersen</h6>
-                                    <p class="m-0">Lorem Ipsum is simply dummy…</p>
-                                </td>
-
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-
-            </Card.Body>
-        </Card>
-    )
+        <tr class="unread" >
+            <td><FaIcons.FaUserCircle className=' fa-5x text-black' /></td>
+            <td>
+                <h6 class="mb-1">{props.name}</h6>
+                <p class="m-0">{props.description}</p>
+            </td>
+        </tr>
+    );
 }
-export default Home
+
+const Home = () => {
+        const history = useHistory();
+        const envio = async () => {
+            await axios.post("http://192.168.1.3:4000/subscription", { "subs": JSON.parse(localStorage.getItem('subs')), "email": localStorage.getItem("correo") })
+        }
+
+        if (localStorage.getItem('correo') === null) {
+            history.push('/login');
+        }
+        else {
+            if (localStorage.getItem('tipo') === '0') {
+                return (<SetType />)
+            }
+            envio();
+        }
+
+        return (
+            <Card className="mx-auto" style={{ width: '20rem' }}>
+                <Card.Header>
+                    <h2 className="fw-bold text-center">Pacientes</h2>
+                </Card.Header>
+                <Card.Body>
+                    <div class="card-block px-0 py-3">
+
+                        <table class="table table-hover">
+                            <tbody>
+
+
+                                <PData name="Isaac" description="Soy un trollazo"></PData>
+
+
+
+                                <tr class="unread">
+                                    <td><FaIcons.FaUserCircle className=' fa-5x text-black' /></td>
+                                    <td>
+                                        <h6 class="mb-1">Mathilde Andersen</h6>
+                                        <p class="m-0">Lorem Ipsum is simply text of…</p>
+                                    </td>
+
+                                </tr>
+                                <tr class="unread">
+                                    <td><FaIcons.FaUserCircle className=' fa-5x text-black' /></td>
+                                    <td>
+                                        <h6 class="mb-1">Karla Sorensen</h6>
+                                        <p class="m-0">Lorem Ipsum is simply…</p>
+                                    </td>
+
+                                </tr>
+                                <tr class="unread">
+                                    <td><FaIcons.FaUserCircle className=' fa-5x text-black' /></td>
+                                    <td>
+                                        <h6 class="mb-1">Ida Jorgensen</h6>
+                                        <p class="m-0">Lorem Ipsum is simply text of…</p>
+                                    </td>
+
+                                </tr>
+                                <tr class="unread">
+                                    <td><FaIcons.FaUserCircle className=' fa-5x text-black' /></td>
+                                    <td>
+                                        <h6 class="mb-1">Albert Andersen</h6>
+                                        <p class="m-0">Lorem Ipsum is simply dummy…</p>
+                                    </td>
+
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                </Card.Body>
+            </Card>
+        );
+    }
+    export default Home
