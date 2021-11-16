6e20563d5c9a628fbff1639640360e714bd98c14
NokiaXD <38729483+NokiaXD@users.noreply.github.com>
2021-11-15 20:50:28 -0500

    configuraciones

--- src/components/P_reg.js
+++ src/components/P_reg.js
@@ -1,44 +1,52 @@
-import React from 'react';
+import React,{useState} from 'react';
 import UploadImage from "./UploadImage";
-import { Row, Col, Image, Button, Form, Card } from 'react-bootstrap';
-import './P_reg.css'
+import { Row, Col, Image as Basura, Button, Form, Card } from 'react-bootstrap';
+
+//cambiar las dimensiones de la imagen en base64 a 100x100 y regresarla como base 64
 
 const P_reg = () => {
 
-    const [image, setImage] = React.useState('');
-
+    const [image, setImage] = useState('');
+    const [datos, setDatos] = useState({}); 
+    const handleChange = (e) => {
+        setDatos({
+            ...datos,
+            [e.target.name]: e.target.value
+        });
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(datos);
+    }
     return(
         <Card className="mx-auto" style={{ width: '20rem'  }}>
             <Card.Header>
             <h2 className="fw-bold text-center">Paciente</h2>
             </Card.Header>
             <Card.Body>
-                <Form>
+                <Form onSubmit={handleSubmit}>
                     <Row>
                         <Col className="text-center">
-                            <Image className="my-1 tamañoImg" src={image? image:'userIcon.png'}  roundedCircle/>
+                            <Basura className="my-1 tamañoImg" src={image? image:'userIcon.png'}  roundedCircle/>
                             <UploadImage image={setImage}/>
+                            
                         </Col>
                     </Row>
                     <Form.Group className="mb-3">
                         <Form.Label>Nombre</Form.Label>
-                        <Form.Control type="text" placeholder="Nombre"/>
+                        <Form.Control type="text" placeholder="Nombre" name="nombre" onChange={handleChange}/>
                     </Form.Group>
                     <Form.Group className="mb-3">
                         <Form.Label>Teléfono</Form.Label>
-                        <Form.Control type="text" placeholder="Teléfono"/>
+                        <Form.Control type="text" placeholder="Teléfono" name="telefono" onChange={handleChange}/>
                     </Form.Group>
                     <Form.Group className="mb-3">
                         <Form.Label>EPS</Form.Label>
-                        <Form.Control type="text" placeholder="EPS"/>
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email EPS</Form.Label>
-                        <Form.Control type="email" placeholder="Email EPS"/>
+                        <Form.Control type="text" placeholder="EPS" name="eps" onChange={handleChange}/>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" placeholder="Email" />
+                        <Form.Control type="email" placeholder="Email" name="email" onChange={handleChange}/>
                     </Form.Group>
                     <div className="text-center">
                     <Button variant="primary" type="submit">
--- src/components/UploadImage.js
+++ src/components/UploadImage.js
@@ -2,6 +2,22 @@ import React, { useState, useEffect } from 'react';
 import { useDropzone } from 'react-dropzone';
 import { Button } from 'react-bootstrap';
 
+const tamañoImg = (image,estados) => {
+    let img = image.replace(/^data:image\/[a-z]+;base64,/, "");
+    let canvas = document.createElement("canvas");
+    let ctx = canvas.getContext("2d");
+    let img_base64 = new Image();
+    img_base64.src = "data:image/jpg;base64," + img;
+    img_base64.onload = function () {
+        canvas.width = 100;
+        canvas.height = 100;
+        ctx.drawImage(img_base64, 0, 0, 100, 100);
+        let dataURL = canvas.toDataURL("image/jpg");
+        estados(dataURL);
+    }
+    
+}
+
 
 const UploadImage = ({ image }) => {
     const [file, setFile] = useState('');
@@ -12,7 +28,7 @@ const UploadImage = ({ image }) => {
         if (file) {
             const fileReader = new FileReader();
             fileReader.onload = () => {
-                image(fileReader.result);
+                tamañoImg(fileReader.result,image)
             }
             fileReader.readAsDataURL(file);
         } else {
