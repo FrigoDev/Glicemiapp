d1cbb0caeaf72ed833593421c206318af342afe7
Isaacllolo <isaaacllolo@gmail.com>
2021-11-16 12:42:49 -0500

    Sidebar segun tipo de usuario

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -3,15 +3,14 @@ import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import * as IoIcons from 'react-icons/io';
 import * as RiIcons from 'react-icons/ri';
-
-export const SidebarData = [
-  {
+const iftipo = () => {
+  const tipo2=[{
     title: 'Pacientes',
     path: '/',
     icon: <FaIcons.FaUserCircle className=' fa-6x' />,
     iconClosed: <RiIcons.RiArrowDownSFill />,
     iconOpened: <RiIcons.RiArrowUpSFill />,
-
+  
     
   },
   {
@@ -20,12 +19,35 @@ export const SidebarData = [
     icon: <FaIcons.FaUserPlus className=' fa-6x ' />,
     iconClosed: <RiIcons.RiArrowDownSFill />,
     iconOpened: <RiIcons.RiArrowUpSFill />,
-
+  
    
   },
   {
     title: 'Cerrar sesion',
     path: '/login',
     icon: <FaIcons.FaDoorClosed className=' fa-6x ' />
+  }];
+  const tipo1=[{
+    title: 'Paciente',
+    path: '/paciente/:cedula',
+    icon: <FaIcons.FaUserCircle className=' fa-6x' />,
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
+  
+    
   },
-];
+  {
+    title: 'Cerrar sesion',
+    path: '/login',
+    icon: <FaIcons.FaDoorClosed className=' fa-6x ' />
+  }];
+ if(localStorage.getItem('tipo')== 1){
+return(
+  tipo1
+  )
+ }
+ else{
+   return(tipo2)
+ }
+  }
+export const SidebarData =  iftipo();
